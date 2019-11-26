4432c4ed4e0133511fcebe96f857b5d30d320fb6
Jan Vavro <vavrojj@gmail.com>
2019-11-27 00:02:49 +0100

    delete user should but not work -_-

--- src/main/java/fit/school/project/iis/mapper/UserMapper.java
+++ src/main/java/fit/school/project/iis/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package fit.school.project.iis.mapper;
 
 import fit.school.project.iis.model.User;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,5 +15,8 @@ public interface UserMapper {
 
     @Select("SELECT * FROM users;")
     List<User> findAll();
+
+    @Delete("DELETE FROM users WHERE id_user = #{id_user}")
+    void deleteUser(@Param("id_user") int id_user);
 
 }
