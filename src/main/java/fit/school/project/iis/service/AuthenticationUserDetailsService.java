1a58ec2ef0f79410b9f02b928639bc9f2df3eb20
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-27 20:53:43 +0100

    Create AuthenticationUserDetailsService.java

---
+++ src/main/java/fit/school/project/iis/service/AuthenticationUserDetailsService.java
@@ -0,0 +1,52 @@
+package fit.school.project.iis.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import fit.school.project.iis.repository.AccountRepository;
+import fit.school.project.iis.model.Account;
+import fit.school.project.iis.model.AccountTransfer;
+
+
+@Service
+public class AuthenticationUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private AccountRepository accountRepository;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Account account = accountRepository.findByUsername(username);
+		if (account == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		return new org.springframework.security.core.userdetails.User(account.getUsername(), account.getPassword(),
+				new ArrayList<>());
+
+		//if ("test".equals(username)) {
+			//return new User("test", "$2a$10$cnVZO74FBXprOVG/Pm3UsOwosdZijOEMzAtv0itI1C2oLBhtMW5f.",
+					//new ArrayList<>());
+		//} else {
+			//throw new UsernameNotFoundException("User not found with username: " + username);
+		//}
+		
+	}
+
+	public Account save(AccountTransfer account) {
+		Account newAccount = new Account();
+		newAccount.setUsername(account.getUsername());
+		newAccount.setPassword(bcryptEncoder.encode(account.getPassword()));
+		return accountRepository.save(newAccount);
+	}
+
+}
