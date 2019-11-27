6138c9c9b23d62a71f213fedc434e1072047e09b
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-27 20:51:05 +0100

    Add files via upload

---
+++ src/main/java/fit/school/project/iis/model/AuthenticationRequest.java
@@ -0,0 +1,38 @@
+package fit.school.project.iis.model;
+
+import java.io.Serializable;
+
+public class AuthenticationRequest implements Serializable {
+    
+    private static final long serialVersionUID = 5926468583005150707L;
+	
+	private String username;
+    private String password;
+    
+    //4 json parsing
+	public AuthenticationRequest()
+	{
+		
+    }
+    
+    public AuthenticationRequest(String username, String password) {
+		this.setUsername(username);
+		this.setPassword(password);
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
