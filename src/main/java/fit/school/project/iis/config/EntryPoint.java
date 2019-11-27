5515c1c8cbe15952a3aeb4e9b8bb3272f8066447
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-27 20:56:36 +0100

    Add files via upload

---
+++ src/main/java/fit/school/project/iis/config/EntryPoint.java
@@ -0,0 +1,23 @@
+package fit.school.project.iis.config;
+
+import org.springframework.stereotype.Component;
+import java.io.Serializable;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Component
+public class EntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final long serialVersionUID = -7858869558953243875L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
