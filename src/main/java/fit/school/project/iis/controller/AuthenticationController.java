81861305671852059ea12c2804333487c440cc3b
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-28 11:54:11 +0100

    Create AuthenticationController.java

---
+++ src/main/java/fit/school/project/iis/controller/AuthenticationController.java
@@ -0,0 +1,71 @@
+package fit.school.project.iis.controller;
+
+import java.util.Objects;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.http.ResponseEntity;
+
+import fit.school.project.iis.model.AuthenticationRequest;
+import fit.school.project.iis.model.AuthenticationResponse;
+import fit.school.project.iis.service.AuthenticationUserDetailsService;
+import fit.school.project.iis.config.AuthenticationTokenUtil;
+import fit.school.project.iis.model.AccountTransfer;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AuthenticationTokenUtil authenticationTokenUtil;
+
+    //@Autowired
+    //private UserDetailsService authenticationInMemoryUserDetailsService;
+
+    @Autowired
+    private AuthenticationUserDetailsService authenticationInMemoryUserDetailsService;
+    
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+            throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = authenticationInMemoryUserDetailsService
+            .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = authenticationTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(token));
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<?> saveAccount(@RequestBody AccountTransfer account) throws Exception {
+		return ResponseEntity.ok(authenticationInMemoryUserDetailsService.save(account));
+	}
+
+    private void authenticate(String username, String password) throws Exception {
+		Objects.requireNonNull(username);
+		Objects.requireNonNull(password);
+
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+}
