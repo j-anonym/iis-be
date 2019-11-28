5e8980cdc4b43bf410bf41cddfb59df307857ea4
vpocoyo <37934230+vpocoyo@users.noreply.github.com>
2019-11-28 20:22:12 +0100

    register

--- src/main/java/fit/school/project/iis/model/Account.java
+++ src/main/java/fit/school/project/iis/model/Account.java
@@ -1,11 +1,11 @@
 package fit.school.project.iis.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
+import java.util.Date;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "accounts")
+@Table(name = "us")
 public class Account {
 
 	@Id
@@ -16,6 +16,14 @@ public class Account {
 	@Column
 	@JsonIgnore
 	private String password;
+	@Column
+	private String name;
+	@Column
+	private String surname;
+	@Column
+	private Date birthdate;
+	@Column
+	private String nationality;
 
 	public String getUsername() {
 		return username;
@@ -32,5 +40,40 @@ public class Account {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getBirthdate() {
+        return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+	}
+	
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+
+ 
 
 }
--- src/main/java/fit/school/project/iis/model/AccountTransfer.java
+++ src/main/java/fit/school/project/iis/model/AccountTransfer.java
@@ -1,8 +1,14 @@
 package fit.school.project.iis.model;
+import java.util.Date;
 
 public class AccountTransfer {
 	private String username;
 	private String password;
+	private String name;
+	private String surname;
+	private String nationality;
+	private Date birthdate;
+
 
 	public String getUsername() {
 		return username;
@@ -18,5 +24,37 @@ public class AccountTransfer {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getBirthdate() {
+        return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+	}
+	
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
 	}
 }
