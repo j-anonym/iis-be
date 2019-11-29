6923476ce8eee291f7d8d91531fd1196380da8ec
Jan Vavro <vavrojj@gmail.com>
2019-11-29 14:11:21 +0100

    edit user

--- src/main/java/fit/school/project/iis/mapper/UserMapper.java
+++ src/main/java/fit/school/project/iis/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import fit.school.project.iis.model.User;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +14,25 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM users where id_user > 1;")
+    @Select("SELECT id_user, id_stat, name, surname, birth, sex, nationality, is_admin, is_left_handed " +
+            "FROM users WHERE id_user > 1;")
     List<User> findAll();
 
     @Delete("DELETE FROM users WHERE id_user = #{id_user}")
     void deleteUser(@Param("id_user") int id_user);
 
+    @Select("SELECT  id_user, id_stat, name, surname, birth, sex, nationality, is_admin, is_left_handed " +
+            "FROM users where id_user = #{id_user};")
+    User findUser(@Param("id_user") int id_user);
+
+    @Update("UPDATE users SET name = #{name}, surname = #{surname}, birth = #{birth}::date, sex = #{sex}::player_type, " +
+            "nationality = #{nationality}, is_admin = #{is_admin}, is_left_handed = #{is_left_handed}" +
+            "WHERE id_user = #{id_user}; ")
+    int updateUser(User user);
 }
+
+//    @Update("UPDATE users SET name = #{name};")
+//    void updateUser(User_tmp user);
+//}
+   
+
--- src/main/java/fit/school/project/iis/resource/UserResource.java
+++ src/main/java/fit/school/project/iis/resource/UserResource.java
@@ -3,6 +3,7 @@ package fit.school.project.iis.resource;
 import fit.school.project.iis.mapper.UserMapper;
 import fit.school.project.iis.model.User;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Component
 @CrossOrigin(origins = "http://localhost:4200")
+@RestController
 @RequestMapping("/api/person")
 public class UserResource {
 
@@ -25,9 +27,30 @@ public class UserResource {
         return userMapper.findAll();
     }
 
+    @RequestMapping(value = "/{id_user}", method = RequestMethod.GET)
+    @ResponseBody
+    public User findUser(@PathVariable("/get/id_user") int id_user) {
+        return userMapper.findUser(id_user);
+    }
+
     @RequestMapping(value = "/delete/{id_user}", method = RequestMethod.DELETE)
     public @ResponseBody
     void deleteUser(@PathVariable("id_user") int id_user) {
         userMapper.deleteUser(id_user);
     }
+
+    @RequestMapping(value = "/update", method = {RequestMethod.POST}, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateUser(@RequestBody User json) {
+        User user = new User(json.getId_user(), json.getId_stat(), json.getName(), json.getSurname(), json.getBirth(),
+                json.getSex(), json.getNationality(), json.is_admin(), json.is_left_handed());
+        int count = userMapper.updateUser(user);
+        System.out.println(count);
+    }
+
+//    @RequestMapping(value = "/update/test", method = {RequestMethod.POST, RequestMethod.GET}, consumes = MediaType.APPLICATION_JSON_VALUE)
+//    @ResponseBody
+//    public void updateUserr(@RequestBody User_tmp json) {
+//        User_tmp user = new User_tmp("som_kokot");
+//        userMapper.updateUser(user);
+//    }
 }
