8948d98995e8b88ccdde530c47ad300d0ce700f0
alesed <xsedla1c@vutbr.cz>
2019-11-29 23:13:54 +0100

    team involved & join API

--- src/main/java/fit/school/project/iis/mapper/TeamMapper.java
+++ src/main/java/fit/school/project/iis/mapper/TeamMapper.java
@@ -1,10 +1,7 @@
 package fit.school.project.iis.mapper;
 
 import fit.school.project.iis.model.Team;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -23,5 +20,14 @@ public interface TeamMapper {
     Team getTeam(@Param("id_team") int id_team);
 
     @Select("SELECT * FROM teams WHERE id_player_1 = ${id_player_1} ORDER BY id_team DESC LIMIT 1")
-    Team getLast(@Param("id_player_1") int id_player_1);
+    int getLast(@Param("id_player_1") int id_player_1);
+
+    @Update("UPDATE teams SET id_player_2=${id_player} WHERE id_team=${id_team}")
+    void joinTeam(@Param("id_team") int id_team, @Param("id_player") int id_player);
+
+    @Delete("DELETE from teams WHERE id_team=${id_team}")
+    void deleteTeam(@Param("id_team") int id_team);
+
+    @Select("SELECT * from teams WHERE id_player_1=${id_player} OR id_player_2=${id_player}")
+    List<Team> getJoined(@Param("id_player") int id_player);
 }
--- src/main/java/fit/school/project/iis/resource/TeamResource.java
+++ src/main/java/fit/school/project/iis/resource/TeamResource.java
@@ -27,9 +27,9 @@ public class TeamResource {
         return teamMapper.getTeam(id_team);
     }
 
-    @RequestMapping(value = "/getlast/{id_player_1}", method= RequestMethod.GET)
-    public @ResponseBody Team getLast(@PathVariable("id_player_1") int id_player_1) {
-        return teamMapper.getLast(id_player_1);
+    @RequestMapping(value = "/getlast/{id_player}", method= RequestMethod.GET)
+    public @ResponseBody int getLast(@PathVariable("id_player") int id_player) {
+        return teamMapper.getLast(id_player);
     }
 
     @RequestMapping(value = "/create", method={RequestMethod.GET, RequestMethod.POST}, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -40,6 +40,21 @@ public class TeamResource {
         );
 
         teamMapper.insertNewTeam(team);
+    }
+
+    @RequestMapping(value = "/join/{id_team}/{id_player}", method= RequestMethod.PUT)
+    public @ResponseBody void joinTeam(@PathVariable("id_team") int id_team, @PathVariable("id_player") int id_player) {
+        teamMapper.joinTeam(id_team, id_player);
+    }
+
+    @RequestMapping(value = "/delete/{id_team}", method= RequestMethod.DELETE)
+    public @ResponseBody void deleteTeam(@PathVariable("id_team") int id_team) {
+        teamMapper.deleteTeam(id_team);
+    }
+
+    @RequestMapping(value = "/getjoined/{id_player}", method= RequestMethod.GET)
+    public @ResponseBody List<Team> getJoined(@PathVariable("id_player") int id_player) {
+        return teamMapper.getJoined(id_player);
     }
 
 }
