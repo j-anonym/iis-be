14d67a6af46eafdd8ee0afbb4fba69a0f91e6492
alesed <xsedla1c@vutbr.cz>
2019-11-30 23:06:40 +0100

    teams, referees and gender API

--- src/main/java/fit/school/project/iis/mapper/TournamentTeamMapper.java
+++ src/main/java/fit/school/project/iis/mapper/TournamentTeamMapper.java
@@ -1,5 +1,6 @@
 package fit.school.project.iis.mapper;
 
+import fit.school.project.iis.model.Team;
 import fit.school.project.iis.model.TournamentTeam;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.annotations.Mapper;
@@ -14,18 +15,26 @@ public interface TournamentTeamMapper {
     @Select("SELECT * FROM team_tournament WHERE id_tournament = ${id_tournament}")
     List<TournamentTeam> getAll(@Param("id_tournament") int id_tournament);
 
-    @Select("SELECT * FROM team_tournament WHERE id_tournament = ${id_tournament} AND is_confirmed = false")
-    List<TournamentTeam> getPendingTeams(@Param("id_tournament") int id_tournament);
+    @Select("SELECT teams.*\n" +
+            "FROM team_tournament\n" +
+            "         JOIN teams ON team_tournament.id_team = teams.id_team\n" +
+            "WHERE id_tournament = ${id_tournament}\n" +
+            "  AND is_confirmed = false")
+    List<Team> getPendingTeams(@Param("id_tournament") int id_tournament);
 
-    @Select("SELECT * FROM team_tournament WHERE id_tournament = ${id_tournament} AND is_confirmed = true")
-    List<TournamentTeam> getAcceptedTeams(@Param("id_tournament") int id_tournament);
+    @Select("SELECT teams.*\n" +
+            "FROM team_tournament\n" +
+            "         JOIN teams ON team_tournament.id_team = teams.id_team\n" +
+            "WHERE id_tournament = ${id_tournament}\n" +
+            "  AND is_confirmed = true")
+    List<Team> getAcceptedTeams(@Param("id_tournament") int id_tournament);
 
     @Insert("INSERT INTO team_tournament(id_team, id_tournament, is_confirmed) VALUES(#{id_team}::integer, #{id_tournament}::integer, false)")
-    void joinTournament(@Param("id_team") int id_team, @Param("id_tournament") int id_tournament);
+    void joinTournament(@Param("id_tournament") int id_tournament, @Param("id_team") int id_team);
 
     @Update("UPDATE team_tournament SET is_confirmed=true WHERE id_team=${id_team} AND id_tournament=${id_tournament}")
-    void acceptTeam(@Param("id_team") int id_team, @Param("id_tournament") int id_tournament);
+    void acceptTeam(@Param("id_tournament") int id_tournament, @Param("id_team") int id_team);
 
     @Delete("DELETE FROM team_tournament WHERE id_team=${id_team} AND id_tournament=${id_tournament}")
-    void declineTeam(@Param("id_team") int id_team, @Param("id_tournament") int id_tournament);
+    void declineTeam(@Param("id_tournament") int id_tournament, @Param("id_team") int id_team);
 }
