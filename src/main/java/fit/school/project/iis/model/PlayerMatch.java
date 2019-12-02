de2fe6000af27d6e0050fbad5d8c3ecc340580b0
alesed <xsedla1c@vutbr.cz>
2019-12-02 11:53:44 +0100

    tree insert result API

--- src/main/java/fit/school/project/iis/model/PlayerMatch.java
+++ src/main/java/fit/school/project/iis/model/PlayerMatch.java
@@ -13,4 +13,18 @@ public class PlayerMatch {
     private Integer id_user_away;
     private Integer id_referee;
     private Integer id_tournament;
+
+    public PlayerMatch (Integer id_player_match, Integer sets_home, Integer sets_away, Integer games_home,
+                         Integer games_away, Integer id_user_home, Integer id_user_away, Integer id_referee,
+                         Integer id_tournament) {
+        this.id_player_match = id_player_match;
+        this.sets_home = sets_home;
+        this.sets_away = sets_away;
+        this.games_home = games_home;
+        this.games_away = games_away;
+        this.id_user_home = id_user_home;
+        this.id_user_away = id_user_away;
+        this.id_referee = id_referee;
+        this.id_tournament = id_tournament;
+    }
 }
--- src/main/java/fit/school/project/iis/model/TeamMatch.java
+++ src/main/java/fit/school/project/iis/model/TeamMatch.java
@@ -13,4 +13,18 @@ public class TeamMatch {
     private Integer id_team_away;
     private Integer id_referee;
     private Integer id_tournament;
+
+    public TeamMatch (Integer id_team_match, Integer sets_home, Integer sets_away, Integer games_home,
+                        Integer games_away, Integer id_team_home, Integer id_team_away, Integer id_referee,
+                        Integer id_tournament) {
+        this.id_team_match = id_team_match;
+        this.sets_home = sets_home;
+        this.sets_away = sets_away;
+        this.games_home = games_home;
+        this.games_away = games_away;
+        this.id_team_home = id_team_home;
+        this.id_team_away = id_team_away;
+        this.id_referee = id_referee;
+        this.id_tournament = id_tournament;
+    }
 }
